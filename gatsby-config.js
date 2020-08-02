83838e64bc18dc8f27b4e3eb6994a7144d8f3783
timschuetz <38264390+timschuetz@users.noreply.github.com>
2020-08-02 11:19:38 -0700

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,6 +68,7 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
+
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
       options: {
